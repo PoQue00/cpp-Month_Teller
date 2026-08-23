4440d8126b11e91ec8285bc79d6efaa7dfb38ade
PoQue__ <farnfieldg@gmail.com>
2026-08-23 16:35:34 +0100

    Update month output messages for clarity

--- Month Teller.cpp
+++ Month Teller.cpp
@@ -8,40 +8,40 @@ int main()
 
     switch (month){
         case 1:
-            std::cout << "January" << '\n';
+            std::cout << "It is January" << '\n';
             break;
         case 2:
-            std::cout << "February" << '\n';
+            std::cout << "It is February" << '\n';
             break;
         case 3:
-            std::cout << "March" << '\n';
+            std::cout << "It is    March" << '\n';
             break;
         case 4:
-            std::cout << "April" << '\n';
+            std::cout << "It is April" << '\n';
             break;
         case 5:
-            std::cout << "May" << '\n';
+            std::cout << "It is May" << '\n';
             break;
         case 6:
-            std::cout << "June" << '\n';
+            std::cout << "It is June" << '\n';
             break;
         case 7:
-            std::cout << "July" << '\n';
+            std::cout << "It is July" << '\n';
             break;
         case 8:
-            std::cout << "August" << '\n';
+            std::cout << "It is August" << '\n';
             break;
         case 9:
-            std::cout << "September" << '\n';
+            std::cout << "It is September" << '\n';
             break;
         case 10:
-            std::cout << "October" << '\n';
+            std::cout << "It is October" << '\n';
             break;
         case 11:
-            std::cout << "November" << '\n';
+            std::cout << "It is November" << '\n';
             break;
         case 12:
-            std::cout << "December" << '\n';
+            std::cout << "It is December" << '\n';
             break;
         default:
             std::cout << "Invalid month number" << '\n';
